6495b6fdaa1312b02605d14796882bd8d98b3b0c
oleg-w570 <ol3g.zor1n@yandex.ru>
2022-10-28 20:18:43 +0300

    оптимизация подсчёта R и M (R пока что не хранится в очереди)

--- methods.py
+++ methods.py
@@ -1,31 +1,58 @@
+import heapq
+
+
 def AGP(func, a, b, r):
     x = [a, b]
-    pairs_x = pairs(x)
-    t = 0
-    while pairs_x[t][1] - pairs_x[t][0] > 1e-4:
-        x.sort()
+    t = 1
+    M = 0
+    indM = 1
+    indNew = 1
+    z_opt_prev = min(func(a), func(b))
+    R = []
+    while x[t] - x[t-1] > 1e-4:
         z = list(map(func, x))
-        pairs_x = pairs(x)
-        pairs_z = pairs(z)
-        M = max(map(funcM, pairs_x, pairs_z))
+
+        changedM = False
+        if indNew == indM:
+            changedM = True
+            for i in range(1, len(x)):
+                tmpM = funcM(x[i-1], x[i], z[i-1], z[i])
+                if tmpM > M:
+                    M = tmpM
+                    indM = i
+        else:
+            for i in range(indNew, indNew+2):
+                tmpM = funcM(x[i - 1], x[i], z[i - 1], z[i])
+                if tmpM > M:
+                    changedM = True
+                    M = tmpM
+                    indM = i
         m = r * M if M > 0 else 1
-        R = list(map(funcR(m), pairs_x, pairs_z))
-        t = R.index(max(R))
-        x_new = (pairs_x[t][1] + pairs_x[t][0]) / 2 - (pairs_z[t][1] - pairs_z[t][0]) / (2 * m)
+
+        z_opt = min(z)
+        if not changedM and z_opt == z_opt_prev:
+            R.pop(indNew-1)
+            for i in range(indNew, indNew+2):
+                tmpR = funcR(m, z_opt, x[i-1], x[i], z[i-1], z[i])
+                R.insert(i-1, tmpR)
+        else:
+            R = []
+            for i in range(1, len(x)):
+                tmpR = funcR(m, z_opt, x[i-1], x[i], z[i-1], z[i])
+                R.append(tmpR)  # heapq.heappush(R, (i, tmpR))
+        t = R.index(max(R))+1
+        z_opt_prev = z_opt
+
+        x_new = (x[t] + x[t-1]) / 2 - (z[t] - z[t-1]) / (2 * m)
         x.append(x_new)
+        x.sort()
+        indNew = x.index(x_new)
     return x
 
 
-def pairs(lst):
-    return [lst[i:i + 2] for i in range(0, len(lst) - 1)]
+def funcM(x1, x2, z1, z2):
+    return abs((z2 - z1) / (x2 - x1))
 
 
-def funcM(pair_x, pair_z):
-    return abs((pair_z[1] - pair_z[0]) / (pair_x[1] - pair_x[0]))
-
-
-def funcR(m):
-    def f(pair_x, pair_z):
-        return m * (pair_x[1] - pair_x[0]) + ((pair_z[1] - pair_z[0]) ** 2) / (m * (pair_x[1] - pair_x[0])) - 2 * (
-                    pair_z[1] + pair_z[0])
-    return f
+def funcR(m, z_opt, x1, x2, z1, z2):
+    return m * (x2 - x1) + ((z2 - z1) ** 2) / (m * (x2 - x1)) - 2 * (z2 + z1 - 2 * z_opt)
