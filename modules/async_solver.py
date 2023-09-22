27b112e184d9cfb0478fdf7f2be9483f5bdab21f
oleg-w570 <ol3g.zor1n@yandex.ru>
2023-09-22 13:43:34 +0300

    async

--- modules/async_solver.py
+++ modules/async_solver.py
@@ -1,5 +1,4 @@
 from multiprocessing import Process, Queue
-from queue import Empty
 
 from modules.core.method import Method
 from modules.utility.interval import Interval
@@ -28,7 +27,6 @@ class Worker(Process):
             new_r = map(self.method.characteristic, new_intrvls)
             new_m = map(self.method.lipschitz_const, new_intrvls)
             self.done.put_nowait((new_m, new_r, new_intrvls))
-            print(f'I am released!')
 
 
 class AsyncSolver(Solver):
@@ -42,52 +40,21 @@ class AsyncSolver(Solver):
         self.workers: list[Worker] = [Worker(self.method, self.tasks, self.done)
                                       for _ in range(self.num_proc)]
 
-    def start_workers(self) -> None:
-        for w in self.workers:
-            w.start()
-        self.tasks.put_nowait((self.trial_data.get_intrvl_with_max_r(),
-                               self.method.m, self.method.optimum))
-
-    def stop_workers(self) -> None:
-        for _ in range(self.num_proc):
-            self.tasks.put_nowait('STOP')
-        self.done.get()
-        for w in self.workers:
-            w.join()
-            w.close()
-            
-    def save_remaining_intrvls(self) -> None:
-        while True:
-            try:
-                _, new_r, new_intrvls = self.done.get_nowait()
-            except Empty:
-                break
-            else:
-                print('remaining interval')
-                for trial in zip(new_r, new_intrvls):
-                    self.trial_data.insert(*trial)
-
-    def get_n_intrvls_with_max_r(self, n: int) -> list[Interval]:
-        intrvls = []
-        for _ in range(n):
-            intrvls.append(self.trial_data.get_intrvl_with_max_r())
-        return intrvls
-
     def solve(self):
         self.first_iteration()
-        self.start_workers()
+        self.sequential_iterations_for_begin()  # вычислим нужное количество интервалов, чтобы загрузить работой все процессы
+        self.start_workers()  # стартуем все процессы и передаём каждому по интервалу
         mindelta: float = float('inf')
         niter: int = 0
         while mindelta > self.stop.eps and niter < self.stop.maxiter:
-            print(f'Iter {niter}')
-            new_m, new_r, new_intrvls = self.done.get()
-            for trial in zip(new_r, new_intrvls):
+            new_m, new_r, new_intrvls = self.done.get()  # обязательно ждём, что один процесс завершит обработку интервала и вернёт результат
+            for trial in zip(new_r, new_intrvls):  # здесь и далее идёт обработка полученных данных
                 self.trial_data.insert(*trial)
             point = new_intrvls[0].right
             self.recalc |= self.method.update_m(max(new_m))
             self.recalc |= self.method.update_optimum(point)
-            released_process = 1
-            while not self.done.empty():
+            released_process = 1  # ставим счётчик процессов, завершивших работу на текущей итерации
+            while not self.done.empty():  # далее проверяем очередь готовых данных, так же обрабатываем их, и увеличиваем счётчик
                 new_m, new_r, new_intrvls = self.done.get()
                 for trial in zip(new_r, new_intrvls):
                     self.trial_data.insert(*trial)
@@ -95,14 +62,48 @@ class AsyncSolver(Solver):
                 self.recalc |= self.method.update_m(max(new_m))
                 self.recalc |= self.method.update_optimum(point)
                 released_process += 1
-            print(f'\treleased process: {released_process}')
-            self.recalculate()
-            old_intrvls = self.get_n_intrvls_with_max_r(released_process)
+            self.recalculate()  # проверяем нужно ли пересчитать характеристики всех интервалов
+            old_intrvls = self.get_n_intrvls_with_max_r(released_process)  # выдаём интервалы с макс характеристикой столько, сколько завершило работу процессов на текущей итерации
             mindelta = min(old_intrvls, key=lambda x: x.delta).delta
             for old_intrvl in old_intrvls:
-                self.tasks.put_nowait((old_intrvl, self.method.m, self.method.optimum))
+                self.tasks.put_nowait((old_intrvl, self.method.m, self.method.optimum))  # также передаём процессам текущие оценку конст Л. и оптимума
             niter += 1
-        self.save_remaining_intrvls()
         self.stop_workers()
         self._solution.accuracy = mindelta
         self._solution.niter = niter
+
+    def start_workers(self) -> None:
+        for w in self.workers:
+            w.start()
+        for _ in range(self.num_proc):
+            self.tasks.put_nowait((self.trial_data.get_intrvl_with_max_r(),
+                                   self.method.m, self.method.optimum))
+
+    def stop_workers(self) -> None:
+        for _ in range(self.num_proc):
+            self.tasks.put_nowait('STOP')
+        while not self.done.empty():
+            _, new_r, new_intrvls = self.done.get()
+            for trial in zip(new_r, new_intrvls):
+                self.trial_data.insert(*trial)
+        for w in self.workers:
+            w.terminate()
+
+    def sequential_iterations_for_begin(self):
+        for _ in range(self.num_proc - 1):
+            old_intrvl: Interval = self.trial_data.get_intrvl_with_max_r()
+            point = self.method.next_point(old_intrvl)
+            new_intrvl = self.method.split_interval(old_intrvl, point)
+            new_m = map(self.method.lipschitz_const, new_intrvl)
+            self.recalc |= self.method.update_m(max(new_m))
+            self.recalc |= self.method.update_optimum(point)
+            self.recalculate()
+            new_r = map(self.method.characteristic, new_intrvl)
+            for trial in zip(new_r, new_intrvl):
+                self.trial_data.insert(*trial)
+
+    def get_n_intrvls_with_max_r(self, n: int) -> list[Interval]:
+        intrvls = []
+        for _ in range(n):
+            intrvls.append(self.trial_data.get_intrvl_with_max_r())
+        return intrvls
--- testing/async_test.py
+++ testing/async_test.py
@@ -9,8 +9,55 @@ from modules.utility.stopcondition import StopCondition
 from problems.gkls_function import GKLSClass, GKLSFunction
 
 
+def all_proc_gkls_op():
+    num_proc = (2, 4, 8)
+    r = 4
+    eps = 0.01
+    iter_counts = []
+    gkls = GKLSFunction()
+    gkls.SetDimension(3)
+    gkls.SetFunctionClass(GKLSClass.Simple, 3)
+    for n in num_proc:
+        for i in range(1, 101):
+            gkls.SetFunctionNumber(i)
+            y_opt = gkls.GetOptimumPoint()
+            z_opt = gkls.GetOptimumValue()
+            problem = Problem(gkls.Calculate, [-1, -1, -1], [1, 1, 1], 3)
+            stop = StopCondition(eps, 100000)
+            param = Parameters(r, n)
+            solver = AsyncSolver(problem, stop, param)
+            solver.solve()
+            sol = solver.solution
+            iter_counts.append(sol.niter)
+            # if sol.optimum.z < z_opt + 9e-2:
+            #     iter_counts.append(sol.niter)
+
+            print(f"GKLS {i}")
+            print(f"Solution point: {y_opt},")
+            print(f"Solution value: {z_opt},")
+            print(f"My point: {sol.optimum.y},")
+            print(f"My value: {sol.optimum.z},")
+            print(f"Iteration count: {sol.niter}")
+            print(f"Number of processes: {n}")
+            print("--------------------------------------")
+        acc = 0
+        percent = []
+        for i in range(0, max(iter_counts) + 1):
+            acc += iter_counts.count(i)
+            percent.append(acc)
+        plt.plot(range(0, max(iter_counts) + 1), percent, linewidth=1, label=f'{n} process')
+
+    plt.style.use('seaborn-v0_8')
+    plt.title(f'Операционная характеристика\nПараллельная асинхронная версия АГП\n'
+              f'Функции GKLS\nr = {r}, eps = {eps}, dim = {3}')
+    plt.xlabel('Количество итераций')
+    plt.ylabel('% решённых задач')
+    plt.legend()
+    plt.show()
+
+
 def gkls_op():
-    n = 4
+    n = 8
     r = 4
     eps = 0.01
     iter_counts = []
@@ -27,7 +74,7 @@ def gkls_op():
         solver = AsyncSolver(problem, stop, param)
         solver.solve()
         sol = solver.solution
-        if sol.optimum.z < z_opt + 9e-2 or i == 92:
+        if sol.optimum.z < z_opt + 9e-2:
             iter_counts.append(sol.niter)
 
         print(f"GKLS {i}")
@@ -45,7 +92,8 @@ def gkls_op():
         percent.append(acc)
 
     plt.style.use('seaborn-v0_8')
-    plt.title(f'Операционная характеристика АГП\nфункции GKLS\nr = {r}, eps = {eps}, dim = {3}')
+    plt.title(f'Операционная характеристика\nПараллельная асинхронная версия АГП\n'
+              f'Функции GKLS\nr = {r}, eps = {eps}, num_proc = {n}, dim = {3}')
     plt.xlabel('Число проведённых испытаний')
     plt.ylabel('% решённых задач')
     plt.plot(range(0, max(iter_counts) + 1), percent, linewidth=1, label='АГП')
