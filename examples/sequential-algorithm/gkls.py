5db2ee232be0cc97df7fbdfe8c5b8bb02a6de1ff
oleg-w570 <ol3g.zor1n@yandex.ru>
2023-09-28 17:59:56 +0300

    ап

--- examples/sequential-algorithm/gkls.py
+++ examples/sequential-algorithm/gkls.py
@@ -1,9 +1,13 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
 from modules.solve import solve
 from problems.gkls.gkls import GKLS
 
 
 if __name__ == "__main__":
-    problem = GKLS(10)
+    problem = GKLS(92)
     print(problem)
     print(
         solve(problem,
